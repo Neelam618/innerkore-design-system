e3336a7d86fd5670d8ab63c49704014f79a0900c
Neelam618 <neelamchavan410@gmail.com>
2021-04-12 18:00:45 +0530

    FooterMini4 and FooterMini5 order

--- src/components/Blocks/FooterMini4.tsx
+++ src/components/Blocks/FooterMini4.tsx
@@ -28,16 +28,16 @@ export const FooterMini4: FC<FooterMini4Types> = ({
 }) => {
     return (
         <div className='lg:flex flex-col lg:flex-row lg:justify-between lg:items-center py-6 px-5 md:px-20 xl:px-40 bg-black'>
-            <div className='lg:mr-8'>
+            <div className='order-1'>
                 <img src={logo} alt={logoAlt} className='w-20 mx-auto'/>
             </div>
-            <div className='text-gray-400 text-sm text-center lg:text-auto mt-6 lg:mt-0'>{copyRightTxt}</div>
-            <div className='flex mt-6 lg:mt-0 justify-center lg:justify-end lg:ml-8'>
+            <div className='flex mt-6 lg:mt-0 justify-center lg:justify-end lg:order-3'>
                 <a href={link1} className={icon1? 'bg-gray-800 inline-block w-6 h-6 rounded-full flex items-center justify-center' : ''} target='_blank'><img src={icon1} className='h-3' /></a>
                 <a href={link2} className={icon2? 'inline-block ml-4 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center' : ''} target='_blank'><img src={icon2} className='h-3' /></a>
                 <a href={link3} className={icon3? 'inline-block ml-4 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center' : ''} target='_blank'><img src={icon3} className='h-3' /></a>
                 <a href={link4} className={icon4? 'inline-block ml-4 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center' : ''} target='_blank'><img src={icon4} className='h-3' /></a>
             </div>
+            <div className='text-gray-400 text-sm text-center lg:text-auto mt-6 lg:mt-0 lg:order-2'>{copyRightTxt}</div>
         </div>
     )
 };
