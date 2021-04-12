62fd5cdf617cdeba2e429f0a8473aeec0fe0f988
Neelam618 <neelamchavan410@gmail.com>
2021-04-12 13:18:09 +0530

    updated FooterMini1 stories

--- src/components/Blocks/FooterMini1.tsx
+++ src/components/Blocks/FooterMini1.tsx
@@ -1,8 +1,4 @@
 import React, { FC } from 'react';
-import Insta from '../../../public/img/instagram.svg';
-import Twitter from '../../../public/img/twitter.svg';
-import Dribble from '../../../public/img/dribble.svg';
-import Youtube from '../../../public/img/youtube.svg';
 
 export type FooterMini1Types = {
 
@@ -10,10 +6,10 @@ export type FooterMini1Types = {
     href?: string;
 
     /** Add icon path for social icons */
-    icon1:string, icon2: string; icon3:string; icon4:string;
+    icon1:string, icon2: string; icon3:string; icon4:string; icon5:string;
     
     /** Add social icon links */
-    link1:string; link2:string; link3: string; link4:string;
+    link1:string; link2:string; link3: string; link4:string; link5:string;
 
     /** Enter copyright text */
     copyRightTxt: string;
@@ -22,8 +18,8 @@ export type FooterMini1Types = {
 
 export const FooterMini1: FC<FooterMini1Types> = ({
     copyRightTxt ='© 2020 Landify UI Kit. All rights reserved',
-    icon1 = Insta, icon2 = Twitter, icon3 = Dribble, icon4 = Youtube,
-    link1 = 'https://instagram.com/', link2 = 'https://twitter.com/', link3 = 'https://dribble.com/', link4 = 'https://youtube.com/', 
+    icon1, icon2, icon3, icon4, icon5,
+    link1, link2, link3, link4, link5
   
 }) => {
     return (
@@ -34,6 +30,7 @@ export const FooterMini1: FC<FooterMini1Types> = ({
                 <a href={link2} className={icon2? 'inline-block ml-4 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center' : ''} target='_blank'><img src={icon2} className='h-3' /></a>
                 <a href={link3} className={icon3? 'inline-block ml-4 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center' : ''} target='_blank'><img src={icon3} className='h-3' /></a>
                 <a href={link4} className={icon4? 'inline-block ml-4 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center' : ''} target='_blank'><img src={icon4} className='h-3' /></a>
+                <a href={link5} className={icon5? 'inline-block ml-4 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center' : ''} target='_blank'><img src={icon5} className='h-3' /></a>
             </div>
         </div>
     )
--- src/stories/Blocks/FooterMini1.stories.tsx
+++ src/stories/Blocks/FooterMini1.stories.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { FooterMini1, FooterMini1Types } from '../../components/Blocks/FooterMini1';
 
+import Insta from '../../../public/img/instagram.svg';
+import Twitter from '../../../public/img/twitter.svg';
+import Dribble from '../../../public/img/dribble.svg';
+import Youtube from '../../../public/img/youtube.svg';
+
+import Github from '../../../public/img/github.svg'
+
 export default {
     title: 'Blocks/FooterMini1',
     component: FooterMini1,
@@ -10,5 +17,28 @@ export const FooterMini1a = (args: FooterMini1Types) => {
     return <FooterMini1 {...args} />;
 };
 FooterMini1a.args = {
-    copyRightTxt: '© 2021 Landify UI Kit. All rights reserved'
+    copyRightTxt: '© 2021 Landify UI Kit. All rights reserved',
+    icon1: Twitter,
+    link1: 'https://twitter.com/',
+    icon2: Insta,
+    link2: 'https://instagram.com/',
+    icon3: Dribble,
+    link3: 'https://dribble.com/',
+    icon4: Youtube,
+    link4: 'https://youtube.com/',
+    icon5: Github,
+    link5: 'https//github.com' 
+};
+
+export const FooterMini1b = (args: FooterMini1Types) => {
+    return <FooterMini1 {...args} />;
+};
+FooterMini1b.args = {
+    copyRightTxt: '© 2021 Landify UI Kit. All rights reserved',
+    icon1: Github,
+    link1: 'https//github.com',
+    icon2: Insta,
+    link2: 'https://instagram.com/',
+    icon3: Dribble,
+    link3: 'https://dribble.com/',
 };
