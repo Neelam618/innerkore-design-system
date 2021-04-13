5e4f1792aabcee6f542ddfa1c29672252e03a77a
Neelam618 <neelamchavan410@gmail.com>
2021-04-13 20:36:50 +0530

    updated FooterMini1 order

--- src/components/Blocks/FooterMini1.tsx
+++ src/components/Blocks/FooterMini1.tsx
@@ -18,7 +18,7 @@ export const FooterMini1: FC<FooterMini1Types> = ({ copyRightTxt, hrefs_with_img
     'bg-gray-800 inline-block w-6 h-6 rounded-full flex items-center justify-center';
   return (
     <div className="lg:flex flex-col lg:flex-row lg:justify-between lg:items-center py-6 px-5 md:px-20 xl:px-40 bg-black">
-      <div className="flex justify-center">
+      <div className="flex justify-center order-1 lg:order-2">
         {hrefs_with_imgSrcs.map(({ href, imgSrc }, index) => {
           if (index === 0) {
             anchorClassName += ' ml-0';
@@ -33,7 +33,7 @@ export const FooterMini1: FC<FooterMini1Types> = ({ copyRightTxt, hrefs_with_img
           );
         })}
       </div>
-      <div className="text-gray-400 text-sm text-center mt-8 lg:mt-0">{copyRightTxt}</div>
+      <div className="text-gray-400 text-sm text-center mt-8 lg:mt-0 order-2 lg:order-1">{copyRightTxt}</div>
     </div>
   );
 };
