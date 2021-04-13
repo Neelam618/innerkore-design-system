46ee33ce62103032bbf59349de3378805a09d446
Neelam618 <neelamchavan410@gmail.com>
2021-04-13 16:46:54 +0530

    updated FooterMini4

--- src/components/Blocks/FooterMini4.tsx
+++ src/components/Blocks/FooterMini4.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 
-export type FooterMini4Types = {
-  /** Add icon path for social icons */
-  icon: string;
+type hrefWithimgSrcType = {
+  href: string;
+  imgSrc: string;
+};
 
-  /** Add social icon links */
-  link: string;
+export type FooterMini4Types = {
 
   /** Enter copyright text */
   copyRightTxt: string;
@@ -13,27 +13,69 @@ export type FooterMini4Types = {
   logo: string;
 
   logoAlt: string;
+
+   /** Add social icon links with img src url */
+   hrefs_with_imgSrcs: hrefWithimgSrcType[];
 };
 
-export const FooterMini4: FC<FooterMini4Types> = ({
-  copyRightTxt,
-  icon,
-  logo,
-  logoAlt,
-}) => {
-  return (
-    <div className="lg:flex flex-col lg:flex-row lg:justify-between lg:items-center py-6 px-5 md:px-20 xl:px-40 bg-black">
+export const FooterMini4: FC<FooterMini4Types> = ({ copyRightTxt, hrefs_with_imgSrcs, logo, logoAlt }) => {
+  let anchorClassName =
+    'bg-gray-800 inline-block w-6 h-6 rounded-full flex items-center justify-center';
+
+  function SocialIcons(){
+    return (
+      <div className="flex mt-6 lg:mt-0 justify-center lg:justify-end lg:ml-8">
+        {hrefs_with_imgSrcs.map(({ href, imgSrc }, index) => {
+          if (index === 0) {
+            anchorClassName += ' ml-0';
+          } else {
+            anchorClassName += ' ml-4';
+          }
+
+          return (
+            <a href={href} className={anchorClassName} target="_blank">
+              <img src={imgSrc} className="h-3" />
+            </a>
+          );
+        })}
+      </div>
+    );
+  }
+
+  function Logo(){
+    return (
       <div className="order-1">
         <img src={logo} alt={logoAlt} className="w-20 mx-auto" />
       </div>
+    );
+  }
 
-      <div className="flex mt-6 lg:mt-0 justify-center lg:justify-end lg:order-3">
-        <a href={link} className={ icon? 'inline-block ml-4 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center': ''}
-          target="_blank"
-        >
-          <img src={icon} className="h-3" />
-        </a>
-      </div>
+  return <FooterMini4Generic
+    copyRightTxt={copyRightTxt}
+    leftComponent={ <Logo /> }
+
+    rightComponent={ <SocialIcons /> }
+    />
+};
+
+type FooterMini4GenericTypes = {
+   /** Enter copyright text */
+   copyRightTxt: string;
+ 
+   leftComponent: React.ReactNode;
+   rightComponent: React.ReactNode;
+};
+
+const FooterMini4Generic: FC<FooterMini4GenericTypes> = ({
+  copyRightTxt,
+  leftComponent,
+  rightComponent
+}) => {
+  return (
+    <div className="lg:flex flex-col lg:flex-row lg:justify-between lg:items-center py-6 px-5 md:px-20 xl:px-40 bg-black">
+      {leftComponent}
+
+      {rightComponent}
 
       <div className="text-gray-400 text-sm text-center lg:text-auto mt-6 lg:mt-0 lg:order-2">
         {copyRightTxt}
--- src/stories/Blocks/FooterMini4.stories.tsx
+++ src/stories/Blocks/FooterMini4.stories.tsx
@@ -17,29 +17,15 @@ export const FooterMini4a = (args: FooterMini4Types) => {
 };
 
 FooterMini4a.args = {
-  icon1: Insta,
-  icon2: Dribble,
-  icon3: Twitter,
-  icon4: Youtube,
-  link1: 'https://instagram.com',
-  link2: 'https://dribble.com',
-  link3: 'https://twitter.com',
-  link4: 'https://youtube.com',
+  hrefs_with_imgSrcs: [
+    { imgSrc: Twitter, href: 'https://twitter.com/' },
+    { imgSrc: Insta, href: 'https://instagram.com/' },
+    { imgSrc: Dribble, href: 'https://dribble.com/' },
+    { imgSrc: Youtube, href: 'https://youtube.com/' },
+  ],
   copyRightTxt: '© 2020 Landify UI Kit. All rights reserved',
   logo: Logo,
   logoAlt: 'Landify',
-};
-export const FooterMini4b = (args: FooterMini4Types) => {
-  return <FooterMini4 {...args} />;
-};
-FooterMini4b.args = {
-  copyRightTxt: '© 2021 Landify UI Kit. All rights reserved',
-  logo: MyLogo,
-  logoAlt: 'my logo alt text',
-  icon1: Insta,
-  link1: 'https://google.com',
-  icon2: Dribble,
-  link2: 'https://dribble.com',
-  icon3: Twitter,
-  link3: 'https://twitter.com',
+  leftComponent: <Logo />,
+  rightComponent: <SocialIcons />
 };
